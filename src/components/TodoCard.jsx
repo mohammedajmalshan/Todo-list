1ca4fb055543873e89879e17197d9d623aa6781f
mohammedajmalshan <ajmalshanmohammed@gmail.com>
2024-09-09 22:16:34 +0530

    commiting todo

---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,24 @@
+
+export default function TodoCard(props) {
+    const { children, handleDeleteTodo, index, handleEditTodo } = props
+    
+  return (
+    
+    <li className="todoItem" > {children}  
+      <div className="actionContainer">
+        <button onClick={()=> {
+          
+          
+          handleEditTodo(index)
+        }}>
+      <i className="fa-regular fa-pen-to-square"></i>
+        </button>
+        <button onClick={()=> {
+          handleDeleteTodo(index)
+        }}>
+      <i className="fa-solid fa-trash"></i>
+        </button>
+      </div>
+    </li>
+  );
+}
